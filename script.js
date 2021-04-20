1b8a74479a379b8ecc92d92c2a8bb10bb0c17b63
kirill <kirill.adamovitch@yandex.ru>
2021-04-20 23:55:01 +0300

    lesson 12

--- script.js
+++ script.js
@@ -1,20 +1,76 @@
-const numberOFilms = +prompt('Cколько фильмов вы уже посмотрели?', '');
+// const numberOFilms = +prompt('Cколько фильмов вы уже посмотрели?', '');
 
 
-const personalMoviedDB = {
-    count: numberOFilms,
-    movies: {},
-    actors:{},
-    genres:[],
-    privat: false
-};
+// const personalMoviedDB = {
+//     count: numberOFilms,
+//     movies: {},
+//     actors:{},
+//     genres:[],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его?',''),  
-      c = prompt('Cколько фильмов вы уже посмотрели?', ''),
-      d = prompt('На сколько оцените его?', ''); 
+// const a = prompt('Один из последних просмотренных фильмов', ''),
+//       b = prompt('На сколько оцените его?',''),  
+//       c = prompt('Cколько фильмов вы уже посмотрели?', ''),
+//       d = prompt('На сколько оцените его?', ''); 
     
-personalMoviedDB.movies[a] = b;
-personalMoviedDB.movies[c] = d;
+// personalMoviedDB.movies[a] = b;
+// personalMoviedDB.movies[c] = d;
 
-console.log(personalMoviedDB);
+// console.log(personalMoviedDB);
+
+// if(4 == 9) {
+// console.log('ОК!');
+// } else{
+//     console.log('Error');
+// }
+
+// const num = 50;
+// if (num < 49) {
+//     console.log('Error');
+// }  else if ( num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok')
+// }
+
+// (num === 50) ? console.log ('Ok!') :  console.log('Error');
+
+// const num = 50;
+// switch(num) {
+//     case 49:
+//         console.log('NEverno');
+//         break;
+//     case 100:
+//         console.log('NEverno');
+//         break;
+//     case 50:
+//         console.log('verno');
+//         break;
+//     default:
+//         console.log('nichego');
+//         break;
+// }
+let num = 50;
+
+// while (num <= 55){
+// console.log(num);
+// num++;
+
+// }
+
+
+// do{
+//     console.log(num);
+//     num++;
+// }
+
+// while (num < 55);
+
+for (let i = 1; i < 10; i++){
+    if(i===6){
+        // break;
+        continue;
+    }
+    console.log(i);
+}
